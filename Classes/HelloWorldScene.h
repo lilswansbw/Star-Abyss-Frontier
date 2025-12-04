175cb07fba0fedea18f2806d9f3bc8b58b38045f
dongzerich <1981829967@qq.com>
2025-12-04 22:11:17 +0800

    Add the generation and movement of enemy planes.

--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -19,6 +19,10 @@ public:
 private:
     cocos2d::Sprite* _bg1;
     cocos2d::Sprite* _bg2;
+
+    cocos2d::Vector<cocos2d::Sprite*> _enemies; // 管理所有敌机的容器
+    void createEnemy(float dt); // 创建单个敌机的函数
+    void removeEnemy(cocos2d::Node* enemy); // 移除敌机的回调,避免内存泄漏
 };
 
 #endif // __HELLOWORLD_SCENE_H__
